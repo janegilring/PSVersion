9c42956b5dce3a6a4029a62194a62dd2a85873a5
Jan Egil Ring <jan.egil.ring@crayon.com>
2016-04-15 08:16:13 +0200

    Added Pester test for Get-PSVersion

---
+++ Tests/Get-PSVersion.Tests.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module $PSScriptRoot\..\PSVersion.psd1 -Force
+
+InModuleScope PSVersion {
+
+Describe 'Get-PSVersion' {
+
+ Context "Desired output" {
+
+    It "Returns a PSCustomObject" {
+        (Get-PSVersion).GetType().FullName | Should Be 'System.Management.Automation.PSCustomObject'
+    }
+
+    }
+
+    Context "Test help examples" {
+
+    It "Tests example 1: Get-PSVersion" {
+    (Get-PSVersion).PSVersion | Should Be $PSVersionTable.PSVersion.ToString()
+    }
+
+    }
+  }
+
+}
