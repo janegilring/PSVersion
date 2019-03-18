84ad327b7656fd5e3c09cd9e3f8fc2380a952a9a
Jan Egil Ring <janegilring@users.noreply.github.com>
2019-03-18 06:51:22 +0100

    Adds support for PowerShell Core !deploy

--- Functions/Get-PSVersion.ps1
+++ Functions/Get-PSVersion.ps1
@@ -1,16 +1,16 @@
-﻿function Get-PSVersion 
+﻿function Get-PSVersion
 {
 
-<#
+    <#
 .SYNOPSIS
     Maps information from $PSVersionTable to a mapping table to retrieve friendly names for Windows PowerShell build numbers
- 
+
 .DESCRIPTION
     Get-PSVersion is a function to map information from $PSVersionTable to a mapping table in order to retrieve friendly names for Windows PowerShell build numbers.
     For example, if the build number is 5.0.10586.117, the returned friendly name will be 'Windows PowerShell 5 RTM 1512'.
     For remote computers, the function relies on PowerShell remoting.
     By default a mapping table bundled with the module is used a data source. The switch parameter -UpdatePSVersionData can be used to download the latest version.
- 
+
 A useful tip is to use a scheduled job to automatically update both the PSVersion module and the PSVersionData:
 $ScheduledJobOption = New-ScheduledJobOption -RunElevated
 $Trigger = New-JobTrigger -At 03:00:00 -Daily
@@ -40,7 +40,7 @@ Update-PSVersionData
 
 .EXAMPLE
      Get-PSVersion -UpdatePSVersionData
- 
+
 .EXAMPLE
      Get-PSVersion -ComputerName 'ComputerA','ComputerB'
 
@@ -52,14 +52,14 @@ Update-PSVersionData
 
 .EXAMPLE
      Get-ADComputer -Filter 'operatingsystem -like "*Server*"' | Select-Object -ExpandProperty Name | Sort-Object -Property Name | Get-PSVersion
- 
+
 .INPUTS
     String
- 
+
 .OUTPUTS
     Selected.System.Collections.Hashtable
     System.Management.Automation.PSCustomObject
- 
+
 .NOTES
     Author:  Jan Egil Ring
     Twitter: @JanEgilRing
@@ -67,152 +67,179 @@ Update-PSVersionData
 
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$false,
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true
-                   )]
-        [string[]]$ComputerName=$env:computername,
+        [Parameter(Mandatory = $false,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [string[]]$ComputerName = $env:computername,
         [System.Management.Automation.Credential()]$Credential = [System.Management.Automation.PSCredential]::Empty,
-                [switch]$UpdatePSVersionData,
+        [switch]$UpdatePSVersionData,
         [parameter(ParameterSetName = "ListVersion")]
         [switch]$ListVersion
     )
 
-    BEGIN {        
+    BEGIN
+    {
         $Params = @{}
 
 
-        If ($PSBoundParameters['Credential']) {
+        If ($PSBoundParameters['Credential'])
+        {
             $Params.Credential = $Credential
         }
 
-        If ($PSBoundParameters['UpdatePSVersionData']) {
+        If ($PSBoundParameters['UpdatePSVersionData'])
+        {
             Update-PSVersionData -ErrorAction SilentlyContinue
         }
 
-            $functionpath = Split-Path -Path ${function:Get-PSVersion}.File
-            $modulepath = Split-Path -Path $functionpath
-            $mappingtablemodulepath = Join-Path -Path $modulepath -ChildPath 'PSVersionMappingTable.json'
+        $functionpath = Split-Path -Path ${function:Get-PSVersion}.File
+        $modulepath = Split-Path -Path $functionpath
+        $mappingtablemodulepath = Join-Path -Path $modulepath -ChildPath 'PSVersionMappingTable.json'
+
+
+        if ($IsLinux)
+        {
+            $mappingtableappdatapath = Join-Path -Path "$($home)/.config" -ChildPath 'PSVersionMappingTable.json'
+        }
+        else
+        {
             $mappingtableappdatapath = Join-Path -Path $env:LOCALAPPDATA\Microsoft\Windows\PowerShell -ChildPath 'PSVersionMappingTable.json'
-            
-            if (Test-Path -Path $mappingtableappdatapath) {
+        }
+
+        if (Test-Path -Path $mappingtableappdatapath)
+        {
 
             Write-Verbose -Message "Found $mappingtableappdatapath, using this version..."
             $mappingtablepath = $mappingtableappdatapath
 
-            } else {
+        }
+        else
+        {
 
             Write-Verbose -Message "$mappingtableappdatapath not found, using the mapping table which is bundled with the module..."
             $mappingtablepath = $mappingtablemodulepath
 
-            }
+        }
 
-            $mappingtable = Get-Content -Path $mappingtablepath -Raw | ConvertFrom-Json
+        $mappingtable = Get-Content -Path $mappingtablepath -Raw | ConvertFrom-Json
 
-            If ($PSBoundParameters['ListVersion']) {
-            
+        If ($PSBoundParameters['ListVersion'])
+        {
+
             Write-Verbose -Message "Parameter Set: ListVersion"
             Write-Verbose -Message "mappingtablepath: $mappingtablepath"
 
             return $mappingtable
             break
 
-             }
+        }
 
-        
-            $ComputerNames = @()
+
+        $ComputerNames = @()
+
+    }
+
+    PROCESS
+    {
+
+        $ComputerNames += $ComputerName
+
+    }
+    END
+    {
+
+        If (-not ($PSBoundParameters['ComputerName']) -or $ComputerName -eq $env:computername -or $ComputerName -eq '127.0.0.1' -or $ComputerName -eq 'localhost' -or $ComputerName -eq '.')
+        {
+
+            Write-Verbose -Message 'Parameter -ComputerName not specified, skipping PS Remoting and retriving information from localhost directly from $PSVersionTable'
+
+            $PSVersion = $PSVersionTable.PSVersion.ToString()
+            $FriendlyName = ($mappingtable | Where-Object {$_.Name -eq $PSVersion}).FriendlyName
+
+
+            if (-not ($FriendlyName))
+            {
+
+                Write-Verbose -Message 'FriendlyName not found in mappingtable, trying to update the local mapping table using Update-PSVersionData...'
+
+                Update-PSVersionData -ErrorAction SilentlyContinue
+
+                $mappingtable = Get-Content -Path $mappingtablepath -Raw | ConvertFrom-Json
+                $FriendlyName = ($mappingtable | Where-Object {$_.Name -eq $PSVersion}).FriendlyName
+
+                if (-not ($FriendlyName))
+                {
+
+                    Write-Verbose -Message 'FriendlyName still not found in mappingtable, setting value to Unknown'
+
+                    $FriendlyName = "Unknown $($_.PSVersion)"
+
+                }
+                else
+                {
+
+                    Write-Verbose -Message 'FriendlyName found after calling Update-PSVersionData'
+
+                }
+            }
+
+
+            $output = [pscustomobject]@{
+                PSComputerName        = $env:ComputerName
+                PSVersion             = $PSVersionTable.PSVersion.ToString()
+                PSVersionFriendlyName = $FriendlyName
+            }
+
+            return $output
 
         }
 
-    PROCESS {
+        $Params.ComputerName = $ComputerNames
 
-        $ComputerNames += $ComputerName
-   
-   }
-   END {
 
-               If (-not ($PSBoundParameters['ComputerName']) -or $ComputerName -eq $env:computername -or $ComputerName -eq '127.0.0.1' -or $ComputerName -eq 'localhost' -or $ComputerName -eq '.') {
-            
-                 Write-Verbose -Message 'Parameter -ComputerName not specified, skipping PS Remoting and retriving information from localhost directly from $PSVersionTable'
-                   
-                   $PSVersion = $PSVersionTable.PSVersion.ToString()
-                   $FriendlyName = ($mappingtable | Where-Object {$_.Name -eq $PSVersion}).FriendlyName
-                  
-                  
-                  if (-not ($FriendlyName)) {
-                  
-                    Write-Verbose -Message 'FriendlyName not found in mappingtable, trying to update the local mapping table using Update-PSVersionData...'
-                    
-                    Update-PSVersionData -ErrorAction SilentlyContinue
-                    
-                    $mappingtable = Get-Content -Path $mappingtablepath -Raw | ConvertFrom-Json
-                    $FriendlyName = ($mappingtable | Where-Object {$_.Name -eq $PSVersion}).FriendlyName
-
-                    if (-not ($FriendlyName)) {
-
-                      Write-Verbose -Message 'FriendlyName still not found in mappingtable, setting value to Unknown'
-
-                      $FriendlyName = "Unknown $($_.PSVersion)"
-
-                    } else {
-                    
-                      Write-Verbose -Message 'FriendlyName found after calling Update-PSVersionData'
-
-                    }
-                  }
-
-                
-                 $output =  [pscustomobject]@{
-                   PSComputerName = $env:ComputerName
-                   PSVersion = $PSVersionTable.PSVersion.ToString()
-                   PSVersionFriendlyName = $FriendlyName
-                 }
-
-                 return $output
-
-               }
-
-   $Params.ComputerName = $ComputerNames
-   
-        
         Invoke-Command @Params {
 
-             $PSVersionTable
+            $PSVersionTable
 
-            } -ErrorAction SilentlyContinue -ErrorVariable failed | Select-Object @{Name='PSComputerName';e={$_.PSComputerName}},@{Name='PSVersion';e={$_.PSVersion.ToString()}},@{Name='PSVersionFriendlyName';e={
+        } -ErrorAction SilentlyContinue -ErrorVariable failed | Select-Object @{Name = 'PSComputerName'; e = {$_.PSComputerName}}, @{Name = 'PSVersion'; e = {$_.PSVersion.ToString()}}, @{Name = 'PSVersionFriendlyName'; e = {
 
-                  $PSVersion = $_.PSVersion.ToString()
-                  $FriendlyName = ($mappingtable | Where-Object {$_.Name -eq $PSVersion}).FriendlyName
-                  
-                  if ($FriendlyName) {
+                $PSVersion = $_.PSVersion.ToString()
+                $FriendlyName = ($mappingtable | Where-Object {$_.Name -eq $PSVersion}).FriendlyName
 
-                  $FriendlyName
+                if ($FriendlyName)
+                {
 
-                  } else {
-
-                  "Unknown $($_.PSVersion)"
-
-                  }
+                    $FriendlyName
 
                 }
-             }
-        
-       if ($failed) {
+                else
+                {
+
+                    "Unknown $($_.PSVersion)"
+
+                }
+
+            }
+        }
+
+        if ($failed)
+        {
 
             foreach ($item in $failed)
             {
 
                 [pscustomobject]@{
-                PSComputerName = $item.TargetObject
-                PSVersion = $null
-                PSVersionFriendlyName = 'N/A - PS Remoting failed'
+                    PSComputerName        = $item.TargetObject
+                    PSVersion             = $null
+                    PSVersionFriendlyName = 'N/A - PS Remoting failed'
 
-            }
+                }
             }
 
 
         }
 
-   }
-    
+    }
+
 }
--- Functions/Update-PSVersionData.ps1
+++ Functions/Update-PSVersionData.ps1
@@ -1,16 +1,16 @@
 ﻿function Update-PSVersionData
 {
 
-<#
+    <#
 .SYNOPSIS
     Updates mapping table used by Get-PSVersion
- 
+
 .DESCRIPTION
     Updates mapping table used by Get-PSVersion, a function to map information from $PSVersionTable in order to retrieve friendly names for Windows PowerShell build numbers
- 
+
 .PARAMETER Uri
     The Uri to download PowerShell version information from.
- 
+
 .EXAMPLE
      Update-PSVersionData
 
@@ -19,13 +19,13 @@
 
 .EXAMPLE
      Update-PSVersionData -Uri 'https://www.domain.com/PSVersionMappingTable.json'
- 
+
 .INPUTS
     String
- 
+
 .OUTPUTS
     None
- 
+
 .NOTES
     Author:  Jan Egil Ring
     Twitter: @JanEgilRing
@@ -33,31 +33,39 @@
 
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$false,
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory = $false,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true)]
         [string]$Uri = 'https://raw.githubusercontent.com/janegilring/PSVersion/master/PSVersionMappingTable.json'
     )
 
-    
-            $DestinationFile = Join-Path -Path $env:LOCALAPPDATA\Microsoft\Windows\PowerShell -ChildPath 'PSVersionMappingTable.json'
-            
-            try {
+    if ($IsLinux)
+    {
+        $DestinationFile = Join-Path -Path "$($home)/.config" -ChildPath 'PSVersionMappingTable.json'
+    }
+    else
+    {
+        $DestinationFile = Join-Path -Path $env:LOCALAPPDATA\Microsoft\Windows\PowerShell -ChildPath 'PSVersionMappingTable.json'
+    }
 
-            Write-Verbose -Message "Trying to download $Uri using Invoke-WebRequest..."
+    try
+    {
 
-            Invoke-WebRequest -Uri $Uri -OutFile $DestinationFile -ErrorAction Stop
+        Write-Verbose -Message "Trying to download $Uri using Invoke-WebRequest..."
 
-            Write-Verbose -Message "$Uri successfully downloaded to $DestinationFile"
+        Invoke-WebRequest -Uri $Uri -OutFile $DestinationFile -ErrorAction Stop
 
-            Write-Verbose -Message "PSVersionData was updated successfully"
+        Write-Verbose -Message "$Uri successfully downloaded to $DestinationFile"
 
-            }
+        Write-Verbose -Message "PSVersionData was updated successfully"
 
-            catch {
+    }
 
-            throw "An error occured while trying to download $Uri to $DestinationFile, exception: $($_.Exception)"
+    catch
+    {
 
-            }
-    
+        throw "An error occured while trying to download $Uri to $DestinationFile, exception: $($_.Exception)"
+
+    }
+
 }
