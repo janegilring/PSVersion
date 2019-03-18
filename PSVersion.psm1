03d197875bdd3f893ad63c1377a11d116a1f456b
Jan Egil Ring <janegilring@users.noreply.github.com>
2019-03-18 21:24:08 +0100

    Adds loading private functions

--- PSVersion.psm1
+++ PSVersion.psm1
@@ -2,5 +2,9 @@
 $ModuleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
 
 Resolve-Path "$ModuleRoot\Functions\*.ps1" | ForEach-Object -Process {
-. $_.ProviderPath
+    . $_.ProviderPath
+}
+
+Resolve-Path "$ModuleRoot\Functions\Private\*.ps1" | ForEach-Object -Process {
+    . $_.ProviderPath
 }
